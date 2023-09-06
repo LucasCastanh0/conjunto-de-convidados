761d4029ce5c39bbf0364f9ba48b2a28275a4a5f
Lucas Phelipe Castanho Ribeiro <pheliperibeiro2003@gmail.com>
2023-09-06 10:29:55 -0300

    Finish Commit

---
+++ src/ConjuntoConvidados.java
@@ -0,0 +1,50 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoConvidados {
+    private Set<Convidado> convidadosSet;
+
+    public ConjuntoConvidados() {
+        this.convidadosSet = new HashSet<>();
+    }
+
+    public void adicionarConvidado(String nome, int codigoConvite) {
+        convidadosSet.add(new Convidado(nome, codigoConvite));
+    }
+
+    public void removerConvidadoPorCodigoConvite(int codigoConvite) {
+        Convidado convidadoParaRemover = null;
+        for (Convidado c : convidadosSet) {
+            if (c.getCodigoConvite() == codigoConvite) {
+                convidadoParaRemover = c;
+                break;
+            }
+        }
+        convidadosSet.remove(convidadoParaRemover);
+    }
+
+    public int contarConvidados() {
+        return convidadosSet.size();
+    }
+
+    public void exibirConvidados() {
+        System.out.println(convidadosSet);
+    }
+
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de convidados");
+
+        conjuntoConvidados.adicionarConvidado("João", 1234);
+        conjuntoConvidados.adicionarConvidado("Maria", 1235);
+        conjuntoConvidados.adicionarConvidado("Pedro", 1235);
+        conjuntoConvidados.adicionarConvidado("Lucas", 1236);
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de convidados");
+
+        conjuntoConvidados.removerConvidadoPorCodigoConvite(1234);
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidado(s) dentro do Set de convidados");
+
+        conjuntoConvidados.exibirConvidados();
+    }
+}
